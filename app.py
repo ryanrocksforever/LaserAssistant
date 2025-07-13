ffecb0af9b20fbe6744a2053ad8073e626e6bcb2
ryanrocksforever <3dmodelingmayarocks@gmail.com>
2025-07-13 13:40:07 -0700

    add voice Func

--- app.py
+++ app.py
@@ -122,4 +122,5 @@ def goto_location(location):
 
 # ────── Run ──────
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, ssl_context=('cert.pem', 'key.pem'))
+
